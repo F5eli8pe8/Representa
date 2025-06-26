e0a31ed75c930823667abedc999d45eccd2333ff
Felipe58 <felipehalk588@gmail.com>
2025-06-26 20:49:25 -0300

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import AroZona from "@/components/EspecAro/AroZona"
 import PluriZona from "@/components/EspecPluri/PluriZona"
 import AegoZona from "@/components/EspecAego/AegoZona"
-import Header from "@/components/Header/Header"
 
 export default function Home (){
 return(
   <div>
-    <Header />
     <AroZona />
     <PluriZona />	
     <AegoZona />
